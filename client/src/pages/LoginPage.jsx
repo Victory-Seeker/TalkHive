63f4fa1ad0e6e105947b01bcd57625175e0c06c7
Victory-Seeker <kundukoustav81@gmail.com>
2025-07-20 17:05:08 +0530

    Update Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -81,7 +81,7 @@ const LoginPage = () => {
 
         <div className='flex flex-col gap-2'>
           {currState === "Sign up" ? (
-            <p className='text-sm text-gray-600'>Already have an account? <span onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+            <p className='text-sm text-white'>Already have an account? <span onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false)}} className='font-medium text-violet-400 cursor-pointer'>Login here</span></p>
           ) : (
             <p className='text-sm text-gray-600'>Create an account <span onClick={()=> setCurrState("Sign up")} className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
           )}
